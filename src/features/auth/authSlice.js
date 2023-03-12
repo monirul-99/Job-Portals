362b5b0b1c01a5693841cfaa13397f0ad854b077
monaulic <sohelmuhammad164@gmail.com>
2023-03-12 20:35:30 +0600

    Job Box Practice

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+
+const initialState = {
+  email: "",
+  role: "",
+  isLoading: true,
+  isError: false,
+  error: "",
+};
+
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }, thunkAPI) => {
+    console.log(email, password);
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data;
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state, payload) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.email = payload;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.email = "";
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default authSlice.reducer;
